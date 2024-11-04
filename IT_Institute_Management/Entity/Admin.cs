edfc2ee3f5bdb5cf2031d62d00c5cf2c552074b1
Hameem Mohamed Imthath <ut01635tic@gmail.com>
2024-11-04 15:03:21 +0530

    Admin Table

---
+++ IT_Institute_Management/Entity/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IT_Institute_Management.Entity
+{
+    public class Admin
+    {
+        [Key]
+        [Required]
+        [MaxLength(50)]
+        public string NIC { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Password { get; set; }
+    }
+}
