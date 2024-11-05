b9045107901698021630037cc8c7c4e6b295dbfe
Hameem Mohamed Imthath <ut01635tic@gmail.com>
2024-11-05 15:03:48 +0530

    Uploaded student get all api and email services

---
+++ IT_Institute_Management/EmailSerivice/EmailService.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace IT_Institute_Management.EmailSerivice
+{
+    public class EmailService : IEmailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string body)
+        {
+            var message = new MailMessage
+            {
+                From = new MailAddress("ut01635tic@gmail.com"),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = false
+            };
+            message.To.Add(new MailAddress(email));
+
+            // SMTP client configuration for Gmail
+            using (var client = new SmtpClient("smtp.gmail.com"))
+            {
+                // Enable SSL and set port for TLS
+                client.EnableSsl = true;
+                client.Port = 587;
+
+                // Set up authentication using your Gmail credentials
+                client.Credentials = new NetworkCredential("ut01635tic@gmail.com", "yourpassword");
+
+                try
+                {
+                    await client.SendMailAsync(message);
+                }
+                catch (Exception ex)
+                {
+                    // Handle any exceptions (e.g., invalid credentials, network issues)
+                    Console.WriteLine($"Error sending email: {ex.Message}");
+                }
+            }
+        }
+    }
+
+}
--- IT_Institute_Management/Repositories/StudentRepository.cs
+++ IT_Institute_Management/Repositories/StudentRepository.cs
@@ -1,8 +1,22 @@
-﻿using IT_Institute_Management.IRepositories;
+﻿using IT_Institute_Management.Database;
+using IT_Institute_Management.Entity;
+using IT_Institute_Management.IRepositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace IT_Institute_Management.Repositories
 {
     public class StudentRepository : IStudentRepository
     {
+        private readonly InstituteDbContext _context;
+
+        public StudentRepository(InstituteDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Student>> GetAllAsync()
+        {
+            return await _context.Students.Include(s => s.Address).Include(e=> e.Enrollments).ToListAsync();
+        }
     }
 }
