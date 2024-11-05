1c7d0ac0c3f2cca6356880620adb77e9837dae94
PPPiragash <pppiragash2004@gmail.com>
2024-11-05 14:07:34 +0530

    Write a Interface define function is GetPaymentByIdAsync in IPaymentRepo

--- IT_Institute_Management/IRepositories/IPaymentRepository.cs
+++ IT_Institute_Management/IRepositories/IPaymentRepository.cs
@@ -5,5 +5,6 @@ namespace IT_Institute_Management.IRepositories
     public interface IPaymentRepository
     {
         Task<IEnumerable<Payment>> GetAllPaymentsAsync();
+        Task<Payment> GetPaymentByIdAsync(Guid id);
     }
 }
