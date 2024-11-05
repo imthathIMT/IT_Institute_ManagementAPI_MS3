51774e36addb3e26e49b3d11a8f54c317dbf89d7
PPPiragash <pppiragash2004@gmail.com>
2024-11-05 14:03:37 +0530

    Add a Constructor in Payment Repository

--- IT_Institute_Management/Repositories/PaymentRepository.cs
+++ IT_Institute_Management/Repositories/PaymentRepository.cs
@@ -1,8 +1,15 @@
-﻿using IT_Institute_Management.IRepositories;
+﻿using IT_Institute_Management.Database;
+using IT_Institute_Management.IRepositories;
 
 namespace IT_Institute_Management.Repositories
 {
     public class PaymentRepository : IPaymentRepository
     {
+        private readonly InstituteDbContext _context;
+
+        public PaymentRepository(InstituteDbContext context)
+        {
+            _context = context;
+        }
     }
 }
