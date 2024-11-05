2add744122c42b5e57c62c67b3f421b5e76fdfdc
PPPiragash <pppiragash2004@gmail.com>
2024-11-05 14:14:22 +0530

    Define a function GetAllPaymentsAsync in IPayment Service

--- IT_Institute_Management/IServices/IPaymentService.cs
+++ IT_Institute_Management/IServices/IPaymentService.cs
@@ -1,6 +1,9 @@
-﻿namespace IT_Institute_Management.IServices
+﻿using IT_Institute_Management.DTO.ResponseDTO;
+
+namespace IT_Institute_Management.IServices
 {
     public interface IPaymentService
     {
+        Task<IEnumerable<PaymentResponseDto>> GetAllPaymentsAsync();
     }
 }
