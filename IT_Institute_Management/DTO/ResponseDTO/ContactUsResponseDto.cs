7bdc4eb9ba18309d8ac6c7c03f3af37f33b9502a
SafeekMohamed18 <ut03211tic@gmail.com>
2024-11-05 16:19:50 +0530

    added DTOs

---
+++ IT_Institute_Management/DTO/ResponseDTO/ContactUsResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace IT_Institute_Management.DTO.ResponseDTO
+{
+    public class ContactUsResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
