3b040a98c3c19f81b2ffd4e11be055027002aae9
PPPiragash <pppiragash2004@gmail.com>
2024-11-05 14:11:27 +0530

    Write a new function GetAllPaymentsAsync is Payment Service

--- IT_Institute_Management/Services/PaymentService.cs
+++ IT_Institute_Management/Services/PaymentService.cs
@@ -1,4 +1,5 @@
-﻿using IT_Institute_Management.IRepositories;
+﻿using IT_Institute_Management.DTO.ResponseDTO;
+using IT_Institute_Management.IRepositories;
 using IT_Institute_Management.IServices;
 
 namespace IT_Institute_Management.Services
@@ -10,6 +11,21 @@ namespace IT_Institute_Management.Services
         public PaymentService(IPaymentRepository paymentRepository)
         {
             _paymentRepository = paymentRepository;
+        }
+
+        public async Task<IEnumerable<PaymentResponseDto>> GetAllPaymentsAsync()
+        {
+            var payments = await _paymentRepository.GetAllPaymentsAsync();
+
+            return payments.Select(p => new PaymentResponseDto
+            {
+                Id = p.Id,
+                Amount = p.Amount,
+                PaymentDate = p.PaymentDate,
+                FullAmount = p.FullAmount,
+                DueAmount = p.DueAmount,
+                EnrollmentId = (Guid)p.EnrollmentId
+            });
         }
     }
 }
