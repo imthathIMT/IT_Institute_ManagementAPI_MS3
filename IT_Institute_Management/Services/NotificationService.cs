855a3b7f3eb2caf1d239315b5f0456ec3541c24e
PPPiragash <pppiragash2004@gmail.com>
2024-11-05 12:20:51 +0530

    Create A Constrctor in Notification Service

--- IT_Institute_Management/Services/NotificationService.cs
+++ IT_Institute_Management/Services/NotificationService.cs
@@ -1,8 +1,15 @@
-﻿using IT_Institute_Management.IServices;
+﻿using IT_Institute_Management.IRepositories;
+using IT_Institute_Management.IServices;
 
 namespace IT_Institute_Management.Services
 {
     public class NotificationService : INotificationService
     {
+        private readonly INotificationRepository _notificationRepository;
+
+        public NotificationService(INotificationRepository notificationRepository)
+        {
+            _notificationRepository = notificationRepository;
+        }
     }
 }
