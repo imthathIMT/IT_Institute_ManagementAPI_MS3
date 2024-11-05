a17bd19d259fc4734a526fdf04049ae28dfdfa1d
PPPiragash <pppiragash2004@gmail.com>
2024-11-05 11:56:39 +0530

    Change in Notification Entity Validation

--- IT_Institute_Management/Entity/Notification.cs
+++ IT_Institute_Management/Entity/Notification.cs
@@ -8,13 +8,14 @@ namespace IT_Institute_Management.Entity
         public Guid Id { get; set; }
 
         [Required(ErrorMessage = "Message is required.")]
+        [MaxLength(500, ErrorMessage = "Message cannot exceed 500 characters.")]
         public string Message { get; set; }
 
         [Required(ErrorMessage = "Date is required.")]
         public DateTime Date { get; set; }
 
-        public string? StudentNIC { get; set; }  // Foreign key to Student
-
+        // Foreign key to Student
+        public string? StudentNIC { get; set; }
         public Student? Student { get; set; }
     }
 }
