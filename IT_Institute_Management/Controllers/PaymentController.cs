3d59e1050539f6e00b73e153e676ea89cc4055f0
PPPiragash <pppiragash2004@gmail.com>
2024-11-05 14:19:25 +0530

    Add a Constructor in PaymentController

--- IT_Institute_Management/Controllers/PaymentController.cs
+++ IT_Institute_Management/Controllers/PaymentController.cs
@@ -1,6 +1,18 @@
-﻿namespace IT_Institute_Management.Controllers
+﻿using IT_Institute_Management.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IT_Institute_Management.Controllers
 {
-    public class PaymentController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
     {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
     }
 }
