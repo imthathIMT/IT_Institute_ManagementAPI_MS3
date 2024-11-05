b12c8c34642067bc500062106f5727d565f192ee
PPPiragash <pppiragash2004@gmail.com>
2024-11-05 12:01:14 +0530

    Notification Entity Create Response and Request DTO

---
+++ IT_Institute_Management/DTO/RequestDTO/NotificationRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace IT_Institute_Management.DTO.RequestDTO
+{
+    public class NotificationRequestDTO
+    {
+        [Required(ErrorMessage = "Message is required.")]
+        public string Message { get; set; }
+
+        [Required(ErrorMessage = "Date is required.")]
+        public DateTime Date { get; set; }
+
+        public string? StudentNIC { get; set; }
+    }
+}
---
+++ IT_Institute_Management/DTO/ResponseDTO/NotificationResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace IT_Institute_Management.DTO.ResponseDTO
+{
+    public class NotificationResponseDTO
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public string StudentNIC { get; set; }
+    }
+}
