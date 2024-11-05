e4e152b8d98a1fff1d1b02c284e4b72ad0d2440b
PPPiragash <pppiragash2004@gmail.com>
2024-11-05 12:22:28 +0530

    Write a function is  GetAllNotificationsAsync is notification interface

--- IT_Institute_Management/IServices/INotificationService.cs
+++ IT_Institute_Management/IServices/INotificationService.cs
@@ -1,6 +1,9 @@
-﻿namespace IT_Institute_Management.IServices
+﻿using IT_Institute_Management.DTO.ResponseDTO;
+
+namespace IT_Institute_Management.IServices
 {
     public interface INotificationService
     {
+        Task<IEnumerable<NotificationResponseDTO>> GetAllNotificationsAsync();
     }
 }
