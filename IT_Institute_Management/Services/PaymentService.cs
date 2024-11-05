733057d56cf79dff3c01e19a1689f98e4279598e
PPPiragash <pppiragash2004@gmail.com>
2024-11-05 14:10:20 +0530

    Add a Constructor in Payment Service

--- IT_Institute_Management/Services/PaymentService.cs
+++ IT_Institute_Management/Services/PaymentService.cs
@@ -1,8 +1,15 @@
-﻿using IT_Institute_Management.IServices;
+﻿using IT_Institute_Management.IRepositories;
+using IT_Institute_Management.IServices;
 
 namespace IT_Institute_Management.Services
 {
     public class PaymentService : IPaymentService
     {
+        private readonly IPaymentRepository _paymentRepository;
+
+        public PaymentService(IPaymentRepository paymentRepository)
+        {
+            _paymentRepository = paymentRepository;
+        }
     }
 }
