d5876b4665c784d3b59a3a1fbc98f6cd65759233
PPPiragash <pppiragash2004@gmail.com>
2024-11-05 12:04:00 +0530

    Write Function GetAllNotificationsAsync in Notification Repo

--- IT_Institute_Management/Repositories/NotificationRepository.cs
+++ IT_Institute_Management/Repositories/NotificationRepository.cs
@@ -1,5 +1,7 @@
 ﻿using IT_Institute_Management.Database;
+using IT_Institute_Management.Entity;
 using IT_Institute_Management.IRepositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace IT_Institute_Management.Repositories
 {
@@ -10,6 +12,11 @@ namespace IT_Institute_Management.Repositories
         public NotificationRepository(InstituteDbContext context)
         {
             _context = context;
+        }
+
+        public async Task<IEnumerable<Notification>> GetAllNotificationsAsync()
+        {
+            return await _context.Notifications.ToListAsync();
         }
     }
 }
