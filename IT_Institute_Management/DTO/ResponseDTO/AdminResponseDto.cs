cd210d658bd87eac744eda1c47135f0fb134cbcd
PPPiragash <pppiragash2004@gmail.com>
2024-11-12 13:45:14 +0530

    Change AdminResponseDTO Filed

--- IT_Institute_Management/DTO/ResponseDTO/AdminResponseDto.cs
+++ IT_Institute_Management/DTO/ResponseDTO/AdminResponseDto.cs
@@ -2,7 +2,12 @@
 {
     public class AdminResponseDto
     {
-            public string NIC { get; set; }
-            public string Password { get; set; }
+        public string NIC { get; set; }
+        public string Name { get; set; }
+        public Guid UserId { get; set; }
+
+        // You can also include additional data that you want to return in the response, such as email, phone, etc.
+        public string Email { get; set; }
+        public string Phone { get; set; }
     }
 }
