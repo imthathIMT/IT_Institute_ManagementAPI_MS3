78fabc96884c42e305e8201e570c56ec8110570a
PPPiragash <pppiragash2004@gmail.com>
2024-11-12 13:21:37 +0530

    Create UserRequestDTO and Add Field

---
+++ IT_Institute_Management/DTO/RequestDTO/UserRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace IT_Institute_Management.DTO.RequestDTO
+{
+    public class UserRequestDto
+    {
+        public string NIC { get; set; }
+        public string Password { get; set; }
+    }
+}
