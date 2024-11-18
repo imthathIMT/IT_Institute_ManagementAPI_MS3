008ee8a872130bc253d052abd2b2e70a9e67a13a
PPPiragash <pppiragash2004@gmail.com>
2024-11-18 08:58:53 +0530

    Replacing FullAmount with TotalPaidAmount in ResponseDTO

--- IT_Institute_Management/DTO/ResponseDTO/PaymentResponseDto.cs
+++ IT_Institute_Management/DTO/ResponseDTO/PaymentResponseDto.cs
@@ -3,10 +3,10 @@
     public class PaymentResponseDto
     {
         public Guid Id { get; set; }
+        public decimal TotalPaidAmount { get; set; }
         public decimal Amount { get; set; }
+        public decimal DueAmount { get; set; }
         public DateTime PaymentDate { get; set; }
-        public decimal FullAmount { get; set; }
-        public decimal DueAmount { get; set; }  
         public Guid EnrollmentId { get; set; }
     }
 }
