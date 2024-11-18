851b4591687ac080f920380332788a274d1d2523
Hameem Mohamed Imthath <ut01635tic@gmail.com>
2024-11-18 18:37:01 +0530

    uploaded login return token

--- IT_Institute_Management/Services/AuthService.cs
+++ IT_Institute_Management/Services/AuthService.cs
@@ -4,6 +4,10 @@ using IT_Institute_Management.Entity;
 using IT_Institute_Management.IRepositories;
 using IT_Institute_Management.IServices;
 using IT_Institute_Management.PasswordService;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace IT_Institute_Management.Services
 {
@@ -11,11 +15,13 @@ namespace IT_Institute_Management.Services
     {
         private readonly IAuthRepository _authRepository;
         private readonly IPasswordHasher _passwordHasher;
+        private readonly IConfiguration _configuration;
 
-        public AuthService(IAuthRepository authRepository,IPasswordHasher passwordHasher)
+        public AuthService(IAuthRepository authRepository,IPasswordHasher passwordHasher, IConfiguration configuration)
         {
             _authRepository = authRepository;
             _passwordHasher = passwordHasher;
+            _configuration = configuration;
         }
 
         public async Task<string> GetLoginUserToken(UserLoginModal request)
@@ -28,13 +34,32 @@ namespace IT_Institute_Management.Services
                 {
                     throw new Exception("Wrong password.");
                 }
-                var token = "Login successfull";
+                var token = CreateToken(user);
                 return token;
             }
             else
             {
                 throw new Exception("User not found");
             }           
+        }
+
+
+
+        private string CreateToken(User user)
+        {
+            var claimsList = new List<Claim>();
+            claimsList.Add(new Claim("nic", user.NIC.ToString()));
+            claimsList.Add(new Claim("Role", user.Role.ToString()));
+
+
+            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]));
+            var credintials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"], _configuration["Jwt:Audience"],
+                claims: claimsList,
+                expires: DateTime.Now.AddDays(10),
+                signingCredentials: credintials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
     }
