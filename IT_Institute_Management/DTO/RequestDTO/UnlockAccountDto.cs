ca5a53209e05a932a3993583310ef09123b26350
ut01635 <ut01635tic@gmail.com>
2024-11-20 13:45:45 +0530

    UnlockAccountDto created

---
+++ IT_Institute_Management/DTO/RequestDTO/UnlockAccountDto.cs
@@ -0,0 +1,8 @@
+﻿namespace IT_Institute_Management.DTO.RequestDTO
+{
+    public class UnlockAccountDto
+    {
+        public string NIC { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
