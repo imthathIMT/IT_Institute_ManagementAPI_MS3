adb9e64d01a2738236c15f0ea8d2acfcd1ed709b
ut01635 <ut01635tic@gmail.com>
2024-11-25 22:14:44 +0530

    update SocialMediaLinks table

--- IT_Institute_Management/Entity/User.cs
+++ IT_Institute_Management/Entity/User.cs
@@ -6,7 +6,6 @@
         public string NIC { get; set; }
         public string Password { get; set; }    
         public Role Role { get; set; }
-
         public Student? Student { get; set; }
         public Admin? Admin { get; set; }
        
