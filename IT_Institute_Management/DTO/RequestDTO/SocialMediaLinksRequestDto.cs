11aa97700a126eea843e597e27d0b6e6a802894e
ut01635 <ut01635tic@gmail.com>
2024-11-25 23:55:05 +0530

    update SocialMediaLinksResponseDto,SocialMediaLinksRequestDto

---
+++ IT_Institute_Management/DTO/RequestDTO/SocialMediaLinksRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IT_Institute_Management.DTO.RequestDTO
+{
+    public class SocialMediaLinksRequestDto
+    {
+        public string? LinkedIn { get; set; }
+        public string? Instagram { get; set; }
+        public string? Facebook { get; set; }
+        public string? GitHub { get; set; }
+        public string? WhatsApp { get; set; }
+
+        [Required(ErrorMessage = "NIC is required.")]
+        [RegularExpression(@"^\d{9}[vxzVXZ]$|^\d{12}$", ErrorMessage = "Invalid NIC format. NIC must either be 9 digits followed by a letter (v/x/z) or 12 digits.")]
+        public string StudentNIC { get; set; }
+    }
+}
---
+++ IT_Institute_Management/DTO/ResponseDTO/SocialMediaLinksResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace IT_Institute_Management.DTO.ResponseDTO
+{
+    public class SocialMediaLinksResponseDto
+    {
+        public Guid Id { get; set; }
+        public string? LinkedIn { get; set; }
+        public string? Instagram { get; set; }
+        public string? Facebook { get; set; }
+        public string? GitHub { get; set; }
+        public string? WhatsApp { get; set; }
+        public string? StudentNIC { get; set; }
+    }
+}
