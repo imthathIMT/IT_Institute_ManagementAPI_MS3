75689462a6aeab8f21d47756c3c298513a472d7d
ut01635 <ut01635tic@gmail.com>
2024-11-28 14:36:03 +0530

    update DTOs for StudentMessage table

---
+++ IT_Institute_Management/DTO/ResponseDTO/StudentMessageResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace IT_Institute_Management.DTO.ResponseDTO
+{
+    public class StudentMessageResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public string StudentNIC { get; set; }
+        public StudentResponseDto Student { get; set; }
+    }
+}
