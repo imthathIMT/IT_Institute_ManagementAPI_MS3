21d3520109016096bb8e5921b6a1a7c655c50779
ut01635 <ut01635tic@gmail.com>
2024-12-01 11:29:44 +0530

    update email services and enquiry reply API

---
+++ IT_Institute_Management/DTO/RequestDTO/EmailRequestDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace IT_Institute_Management.DTO.RequestDTO
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class EmailRequestDTO
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address format")]
+        public string Email { get; set; }  // Email address of the recipient
+
+        [Required(ErrorMessage = "Body is required")]
+        [StringLength(500, ErrorMessage = "Body cannot exceed 500 characters")]
+        public string Body { get; set; }   // Body of the email
+
+        [Required(ErrorMessage = "Subject is required")]
+        [StringLength(100, ErrorMessage = "Subject cannot exceed 100 characters")]
+        public string Subject { get; set; } // Subject of the email
+    }
+
+}
